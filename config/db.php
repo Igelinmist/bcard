d41201b9893625d9aaf6396a661969acc3ef3683
Vasiliy Troyan <trovas@inbox.ru>
2015-02-24 16:27:39 +0600

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2bcard',
+    'username' => 'yiiuser',
+    'password' => 'pa$$0rd',
+    'charset' => 'utf8',
+];
