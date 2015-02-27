7df01a4fc2889bad4d3bb1b933913a1ee126220d
Vasiliy Troyan <trovas@inbox.ru>
2015-02-27 13:30:24 +0600

    Complit model GeneratorForm with termInMonth

--- models/GeneratorForm.php
+++ models/GeneratorForm.php
@@ -6,13 +6,16 @@ use yii\base\Model;
 
 class GeneratorForm extends Model
 {
+    /* Input params for CardsGenerator */
     public $serNo;
     public $amount;
+    public $termInMounth;
 
     public function rules()
     {
       return [
-        [['serNo','amount'], 'required'],
+        [['serNo','amount','termInMounth'], 'required'],
+        [['amount','termMounth'], 'integerOnly'],
       ];
     }
 }
